1c65480ed5f3e00b91a6c462eec833a1b4f6330a
OlenaShyrina <ev.shyrina@gmail.com>
2022-12-01 21:33:28 +0200

    Update task-08.js
    New changes

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    const {elements: {email, password}} = event.currentTarget;
+
+event.preventDefault();
+
+if (email.value === '' || password.value === '') {
+    alert('Please, foll in all fields of the form!');
+
+} else {
+    const formData = {
+        email: email.value,
+        password: password.value,
+    };
+    console.log(formData);
+}
+
+event.currentTarget.removeEventListener();
+}
 
