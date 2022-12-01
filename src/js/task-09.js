c786773444662febb98c5c9b9f4c6423f5ae74ac
OlenaShyrina <ev.shyrina@gmail.com>
2022-12-01 21:33:33 +0200

    Update task-09.js
    New changes

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
+
+const changeColorBtn = document.querySelector(".change-color");
+const nameOfColor = document.querySelector(".color");
+const body = document.querySelector("body");
+
+nameOfColor.textContent = getRandomHexColor();
+changeColorBtn.addEventListener("click", getRandomHexColor);
+body.style.backgroundColor = nameOfColor.textContent;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
