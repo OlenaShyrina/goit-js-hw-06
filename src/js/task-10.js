ff33af246467d9ebfb4892cbf2ab77304e44bfcf
OlenaShyrina <ev.shyrina@gmail.com>
2022-12-01 21:33:36 +0200

    Update task-10.js
    New changes

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnDestroy = document.querySelector('[data-destroy]');
+const divBoxes = document.querySelector('#boxes');
+const divControls = document.querySelector('#controls');
+const amount = document.querySelector('input');
+console.log(amount);
+
+const boxesCreated = [];
+
+buttonCreate.addEventListener('input', boxesCreated);
+
+function createBoxes(amount){
+for(let i=0; i <= amount; i +=1){
+const box = document.querySelector('div');
+box.style.width = String(30 + 10 * i) + 'px';
+box.style.height = String(30 + 10 * i) + 'px';
+box.style.backgroundColor = getRandomHexColor;
+boxesCreated.push(box);
+}
+
+divBoxes.append(...boxesCreated);
+}
